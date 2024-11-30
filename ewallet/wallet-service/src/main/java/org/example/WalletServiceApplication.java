e8361a653b8fac858496161d0ebd4ffdeb62f21e
Sheikh Ahmed <sheikhishtiak@gmail.com>
2024-11-30 00:00:26 -0500

    user and wallet service initialized

---
+++ ewallet/wallet-service/src/main/java/org/example/WalletServiceApplication.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+
+public class WalletServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WalletServiceApplication.class);
+    }
+}
