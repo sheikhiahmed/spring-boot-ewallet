697556971021a0641e93dd3fad679439835bbf3f
Sheikh Ahmed <sheikhishtiak@gmail.com>
2024-11-30 10:53:37 -0500

    user dto initialized

---
+++ ewallet/user-service/src/main/java/org/example/dto/GetUserResponse.java
@@ -0,0 +1,24 @@
+package org.example.dto;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GetUserResponse {
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    private int age;
+
+    private Date createdOn;
+
+    private Date updatedOn;
+}
